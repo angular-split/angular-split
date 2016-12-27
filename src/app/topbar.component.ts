85b7d401b4bfe6a518db652a40f5d4d60cf18395
bertrandg <bertrandgaillard@hotmail.fr>
2016-12-27 18:57:04 +0100

    up

--- src/app/topbar.component.ts
+++ src/app/topbar.component.ts
@@ -6,29 +6,49 @@ import { Router } from '@angular/router'
   styles: [`
     a.navbar-brand,
     a.navbar-brand:hover {
-        font-weight: bold;
-        color: #ffc421;
+      font-weight: bold;
+      color: #ffc421;
+    }
+
+    .navbar-toggler {
+      float: right;
     }
   `],
-  template: `
-    <nav class="navbar navbar-fixed-top navbar-dark bg-inverse">
-      <a class="navbar-brand" href="#">angular-split</a>
-      <ul class="nav navbar-nav">
-        <li class="nav-item" [class.active]="router.isActive('/', true)">
-          <a class="nav-link" routerLink="/">Home</a>
-        </li>
-        <li class="nav-item" [class.active]="router.isActive('/documentation', true)">
-          <a class="nav-link" routerLink="/documentation">Documentation</a>
-        </li>
-        <li class="nav-item" [class.active]="router.isActive('/examples', true)">
-          <a class="nav-link" routerLink="/examples">Examples</a>
-        </li>
-        <li class="nav-item" [class.active]="router.isActive('/advanced-example', true)">
-          <a class="nav-link" routerLink="/advanced-example">Advanced example</a>
-        </li>
-      </ul>
-    </nav>`
+  host: {
+    'class': 'navbar navbar-fixed-top navbar-dark bg-inverse' 
+  },
+  template:  `
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button class="navbar-toggler hidden-lg-up" 
+                        (click)="isCollapsed = !isCollapsed">
+                    <span class="icon-bar"></span> 
+                    <span class="icon-bar"></span> 
+                    <span class="icon-bar"></span> 
+                </button> 
+                <a class="navbar-brand" href="#">angular-split</a>
+            </div>
+            <div class="clearfix hidden-lg-up"></div>
+            <nav class="collapse navbar-toggleable-md" [collapse]="isCollapsed">
+                <ul class="nav navbar-nav">
+                    <li class="nav-item" [class.active]="router.isActive('/', true)">
+                      <a class="nav-link" routerLink="/">Home</a>
+                    </li>
+                    <li class="nav-item" [class.active]="router.isActive('/documentation', true)">
+                      <a class="nav-link" routerLink="/documentation">Documentation</a>
+                    </li>
+                    <li class="nav-item" [class.active]="router.isActive('/examples', true)">
+                      <a class="nav-link" routerLink="/examples">Examples</a>
+                    </li>
+                    <li class="nav-item" [class.active]="router.isActive('/advanced-example', true)">
+                      <a class="nav-link" routerLink="/advanced-example">Advanced example</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>`
 })
 export class TopbarComponent {
+    isCollapsed: boolean = true
+
     constructor(public router: Router) {}
 }
