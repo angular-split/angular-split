1088e856896b1f5e67feb0d134b012aaa71944f6
bertrandg <bertrandgaillard@hotmail.fr>
2018-11-10 14:15:42 +0100

    Add Cypress E2E env and first test for local & ci

---
+++ cypress/integration/basic.spec.js
@@ -0,0 +1,29 @@
+/// <reference types="Cypress" />
+
+context('Direction change', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:4200/#/examples/simple-split')
+  })
+
+  it('cy.viewport() - set the viewport size and dimension', () => {
+    cy.viewport(800, 600);
+
+    const W = 650;
+    const H = 300;
+    const GUTTER = 11;
+
+    cy.get('as-split').should('have.css', 'flex-direction', 'row');
+    cy.get('as-split-area[size="30"]').should('have.css', 'height', `${ H }px`);
+    cy.get('as-split-area[size="30"]').should('have.css', 'flex', '0 0 calc(-3.3px + 30%)');
+    cy.get('as-split-area[size="70"]').should('have.css', 'height', `${ H }px`);
+    cy.get('as-split-area[size="70"]').should('have.css', 'flex', '0 0 calc(-7.7px + 70%)');
+
+    cy.get('.btns > .btn').click();
+
+    cy.get('as-split').should('have.css', 'flex-direction', 'column');
+    cy.get('as-split-area[size="30"]').should('have.css', 'width', `${ W }px`);
+    cy.get('as-split-area[size="30"]').should('have.css', 'flex', '0 0 calc(-3.3px + 30%)');
+    cy.get('as-split-area[size="70"]').should('have.css', 'width', `${ W }px`);
+    cy.get('as-split-area[size="70"]').should('have.css', 'flex', '0 0 calc(-7.7px + 70%)');
+  })
+})
