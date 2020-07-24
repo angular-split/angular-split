4787ff3729ae0d6795210ea333205e8f2db5bdf2
Bram Borggreve <borggreve@gmail.com>
2020-07-24 11:19:28 -0500

    chore: update credits section in README and homepage

--- README.md
+++ README.md
@@ -7,6 +7,12 @@ Documentation and examples:
 
 ### Credits
 
+#### Built by [Bertrand Gaillard](https://github.com/bertrandg).
+
+#### Maintained by: [Bram Borggreve](https://github.com/beeman) (As of 2020).
+
+#### Previously: [Bertrand Gaillard](https://github.com/bertrandg) (2016 to 2019)
+
 Crossbrowser testing sponsored by [Browser Stack](https://www.browserstack.com)
 [<img src="https://camo.githubusercontent.com/a7b268f2785656ab3ca7b1cbb1633ee5affceb8f/68747470733a2f2f64677a6f7139623561736a67312e636c6f756466726f6e742e6e65742f70726f64756374696f6e2f696d616765732f6c61796f75742f6c6f676f2d6865616465722e706e67" alt="Browser Stack" height="31px" style="background: cornflowerblue;">](https://www.browserstack.com)
 
--- src_app/app/component/home/home.route.component.ts
+++ src_app/app/component/home/home.route.component.ts
@@ -26,42 +26,49 @@ import { Component } from '@angular/core'
       }
     `,
   ],
-  template: ` <div class="container">
-    <div class="jumbotron">
-      <h1>
-        angular-split
-        <a href="https://www.npmjs.com/package/angular-split"
-          ><img
-            src="https://img.shields.io/npm/v/angular-split/latest.svg?style=flat-square"
-            alt="npm latest version"
-            height="18" /></a
-        >&nbsp;<a href="https://www.npmjs.com/package/angular-split"
-          ><img
-            src="https://img.shields.io/npm/v/angular-split/next.svg?style=flat-square"
-            alt="npm next version"
-            height="18" /></a
-        >&nbsp;<a href="https://travis-ci.com/angular-split/angular-split"
-          ><img
-            src="https://img.shields.io/travis/com/angular-split/angular-split/master.svg?style=flat-square&label=Cypress+E2E+tests"
-            alt="Build Status"
-            height="18"
-        /></a>
-      </h1>
-      <h4>Angular UI library to split views and allow dragging to resize areas using CSS flexbox layout.</h4>
-      <br />
-      <h5>Install npm module:</h5>
-      <pre [innerText]="code1"></pre>
-      <br />
-      <h5>Add angular module to your app:</h5>
-      <pre [innerText]="code2"></pre>
-      <br />
-      <h5>Bug report:</h5>
-      <p>
-        If you find a bug, open an issue with a
-        <a href="https://stackblitz.com/fork/angular-split-demo" target="_blank">StackBlitz</a> demo showing it.
-      </p>
+  template: `
+    <div class="container">
+      <div class="jumbotron">
+        <h1>
+          angular-split
+          <a href="https://www.npmjs.com/package/angular-split"
+            ><img
+              src="https://img.shields.io/npm/v/angular-split/latest.svg?style=flat-square"
+              alt="npm latest version"
+              height="18" /></a
+          >&nbsp;<a href="https://www.npmjs.com/package/angular-split"
+            ><img
+              src="https://img.shields.io/npm/v/angular-split/next.svg?style=flat-square"
+              alt="npm next version"
+              height="18" /></a
+          >&nbsp;<a href="https://travis-ci.com/angular-split/angular-split"
+            ><img
+              src="https://img.shields.io/travis/com/angular-split/angular-split/master.svg?style=flat-square&label=Cypress+E2E+tests"
+              alt="Build Status"
+              height="18"
+          /></a>
+        </h1>
+        <h4>Angular UI library to split views and allow dragging to resize areas using CSS flexbox layout.</h4>
+        <br />
+        <h5>Install npm module:</h5>
+        <pre [innerText]="code1"></pre>
+        <br />
+        <h5>Add angular module to your app:</h5>
+        <pre [innerText]="code2"></pre>
+        <br />
+        <h5>Bug report:</h5>
+        <p>
+          If you find a bug, open an issue with a
+          <a href="https://stackblitz.com/fork/angular-split-demo" target="_blank">StackBlitz</a> demo showing it.
+        </p>
+        <h5>Credits:</h5>
+        <p>
+          This library is built by <a href="https://github.com/bertrandg">Bertrand Gaillard</a> and maintained by
+          <a href="https://github.com/beeman">Bram Borggreve</a>.
+        </p>
+      </div>
     </div>
-  </div>`,
+  `,
 })
 export class HomeComponent {
   code1: string = `npm install angular-split`
