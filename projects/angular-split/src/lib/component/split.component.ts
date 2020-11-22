a9ce0368efa614b9b57f6f769e5ab9599aacb297
Shemesh, Ofir <ofir_shemesh@bmc.com>
2020-11-22 10:45:28 +0200

    NEW expand collapse code & examples

--- projects/angular-split/src/lib/component/split.component.ts
+++ projects/angular-split/src/lib/component/split.component.ts
@@ -273,6 +273,8 @@ export class SplitComponent implements AfterViewInit, OnDestroy {
       size: 0,
       minSize: null,
       maxSize: null,
+      sizeBeforeCollapse: null,
+      gutterBeforeCollapse: 0
     }
 
     if (component.visible === true) {
@@ -765,5 +767,42 @@ export class SplitComponent implements AfterViewInit, OnDestroy {
 
   public ngOnDestroy(): void {
     this.stopDragging()
+  }
+
+  public collapseArea(comp: SplitAreaDirective, newSize: number, gutter: 'left'|'right'): void {
+
+    const area = this.displayedAreas.find(a => a.component === comp);
+    if (area === undefined) {
+      return;
+    }
+    const whichGutter = gutter === 'right' ? 1 : -1;
+    if (!area.sizeBeforeCollapse) {
+      area.sizeBeforeCollapse = area.size;
+      area.gutterBeforeCollapse = whichGutter;
+    }
+    area.size = newSize;
+    const gtr = this.gutterEls.find(f => f.nativeElement.style.order === `${area.order + whichGutter}`);
+    if (gtr) {
+      this.renderer.addClass(gtr.nativeElement, 'as-split-gutter-collapsed');
+    }
+    this.updateArea(comp, false, false);
+  }
+
+  public expandArea(comp: SplitAreaDirective): void {
+
+    const area = this.displayedAreas.find(a => a.component === comp);
+    if (area === undefined) {
+      return;
+    }
+    if (!area.sizeBeforeCollapse) {
+      return;
+    }
+    area.size = area.sizeBeforeCollapse;
+    area.sizeBeforeCollapse = null;
+    const gtr = this.gutterEls.find(f => f.nativeElement.style.order === `${area.order + area.gutterBeforeCollapse}`);
+    if (gtr) {
+      this.renderer.removeClass(gtr.nativeElement, 'as-split-gutter-collapsed');
+    }
+    this.updateArea(comp, false, false);
   }
 }
