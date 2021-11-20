2f28ece9019046271ea08d2aa8dbc0ec582e3807
Bram Borggreve <borggreve@gmail.com>
2021-11-20 00:34:21 +0000

    fix: export internal interfaces

--- projects/angular-split/src/public_api.ts
+++ projects/angular-split/src/public_api.ts
@@ -5,4 +5,4 @@
 export { AngularSplitModule } from './lib/module'
 export { SplitComponent } from './lib/component/split.component'
 export { SplitAreaDirective } from './lib/directive/split-area.directive'
-export { IOutputData, IOutputAreaSizes } from './lib/interface'
+export * from './lib/interface'
